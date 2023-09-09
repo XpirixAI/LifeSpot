93d500d8d3cfc0b1ea04723fdae7b90f4320916e
Colton Grady <coltongrady@Coltons-MacBook-Pro.local>
2023-09-09 16:42:28 -0700

    loading jquery securely

--- resources/views/lifespot_layout.blade.php
+++ resources/views/lifespot_layout.blade.php
@@ -32,5 +32,5 @@
 
   <!--WHAT IS THIS SCRIPT FOR ?  OS IT REALLY NEEDED????-->
   {{-- <script src="https://cdn.jsdelivr.net/npm/tw-elements/dist/js/index.min.js"></script> --}}
-  <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js" type="text/javascript"></script>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js" type="text/javascript"></script>
 </x-app-layout>
