8dbffaa8c63ae1f7d29883b70997ee9fbe5971a1
Colton Grady <coltongrady@Coltons-MacBook-Pro.local>
2023-09-09 15:56:20 -0700

    adding missing files from last commit

---
+++ resources/views/coming_soon.blade.php
@@ -0,0 +1,9 @@
+<x-coming-soon>
+    <h1>COMING SOON!</h1>
+    <form method="get" action="{{route('temp.dev.pass')}}">
+        <label class="block" for="password">Developer Password</label>
+        <input class="block" class="block" id="password" type="text" name="password"/>
+
+        <button type="submit" class="">Submit</button>
+    </form>
+</x-coming-soon>
---
+++ resources/views/components/coming-soon.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+
+        <title>{{ config('app.name', 'Laravel') }}</title>
+
+        <!-- Fonts -->
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css">
+
+       <!-- https://tailwind-elements.com/quick-start/-->
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/tw-elements/dist/css/index.min.css" />
+        <!-- Styles -->
+        @livewireStyles
+        <style>
+            [x-cloak] { display: none; }
+        </style>
+
+        <!-- Scripts -->
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
+    </head>
+    <body class="font-sans antialiased bg-white xg-[#faf6f5]">
+        <div class="min-h-screen bg-white">
+            {{-- @livewire('navigation-menu') --}}
+            @include('navigation-landing-menu')
+
+            <!-- Page Heading -->
+            {{-- @if (isset($header))
+                <header class="bg-white shadow">
+                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+                        {{ $header }}
+                    </div>
+                </header>
+            @endif --}}
+
+            <!-- Page Content -->
+            <main>
+                {{ $slot }}
+            </main>
+        </div>
+        <script src="https://cdn.jsdelivr.net/npm/tw-elements/dist/js/index.min.js"></script>
+        @stack('modals')
+
+        @livewireScripts
+    </body>
+</html>
