74ab8bb169b7e9267e58f83c0546412d7268b05f
Colton Grady <coltongrady@Coltons-MacBook-Pro.local>
2023-10-23 22:37:13 -0700

    only accepting correct image file types for bank images

--- resources/views/lifespot/myestate/financials/banking/edit.blade.php
+++ resources/views/lifespot/myestate/financials/banking/edit.blade.php
@@ -45,7 +45,14 @@
                         </svg>
 
                     </label>
-                    <input xclass="rounded-lg max-h-full h-auto w-full" type="file" name="thumbnail" id="image" value="'{{ old('thumbnail') }}'">
+                    <input 
+                        xclass="rounded-lg max-h-full h-auto w-full" 
+                        type="file" 
+                        name="thumbnail" 
+                        id="image" 
+                        value="'{{ old('thumbnail') }}'"
+                        accept=".jpeg,.pdf,.jpg,.webp"
+                    />
                     @error('thumbnail')
                         <p class="text-red-500 text-xs mt-2">{{ $message }}</p>
                     @enderror
